2181e4d2b40f82abbf43feadf12d13413f6a2d4a
prasannarajaram <prasanna.uralsk@gmail.com>
2024-05-23 15:22:29 +0530

    Added abort/compl etc part to six bit task

--- c_six_bit_task.py
+++ c_six_bit_task.py
@@ -8,4 +8,12 @@ def create_sbt(site):
     sbt_list = ''
     for name in sbt_name_list:
         sbt_list = sbt_list + f'"SixBitTask","{name}","{name}","","{site}"' +'\n'
+        sbt_list = sbt_list + f'"{name}","Abort","False","","{site}","NodeId","ns=1;s=t|{name}Abort","CyclicContinuous","Ti1s","None",""' + '\n'
+        sbt_list = sbt_list + f'"{name}","Compl","False","","{site}","NodeId","ns=1;s=t|{name}Compl","CyclicContinuous","Ti1s","None",""' + '\n'
+        sbt_list = sbt_list + f'"{name}","Pause","False","","{site}","NodeId","ns=1;s=t|{name}Pause","CyclicContinuous","Ti1s","None",""' + '\n'
+        sbt_list = sbt_list + f'"{name}","Progr","False","","{site}","NodeId","ns=1;s=t|{name}Progr","CyclicContinuous","Ti1s","None",""' + '\n'
+        sbt_list = sbt_list + f'"{name}","Start","False","","{site}","NodeId","ns=1;s=t|{name}Start","CyclicContinuous","Ti1s","None",""' + '\n'
     return sbt_list
+
+
+
